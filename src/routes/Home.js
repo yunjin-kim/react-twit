50daa8d3a49ef5eaedf391e0c342e41e9921466c
(yunjin) <sion11290805@gmail.com>
2021-12-02 21:06:37 +0900

    feat: firebase 데이터베이스에 트윗기능

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,36 @@
-import React from "react";
+import { addDoc, collection } from '@firebase/firestore';
+import React, { useState } from 'react'
+import { dbService } from '../fbase';
 
-const Home = () => <span>Home</span>;
+export default function Home() {
+  const [twit, setTwit] = useState("");
 
-export default Home;
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setTwit(value);
+
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "twit"), {
+        twit,
+        createAt: Date.now()
+      })
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    setTwit("");
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={twit} onChange={onChange} placeholder="What?" maxLength={120} />
+        <input type="submit" value="twit" />
+      </form>
+    </>
+  )
+}
